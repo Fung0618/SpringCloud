557e03e620418c0a333f3a5a0643243f0e687ed2
Fung0618 <358052636@qq.com>
2021-05-13 23:29:56 +0800

    master的第四次修改

--- cloudalibaba-customer-order83/src/main/java/com/atguigu/springcloud/alibaba/controller/OrderController.java
+++ cloudalibaba-customer-order83/src/main/java/com/atguigu/springcloud/alibaba/controller/OrderController.java
@@ -41,7 +41,7 @@ public class OrderController {
         return "dev2";
     }
 
-    @GetMapping(value = "/test03")
+    @GetMapping(value = "/test04")
     public String test01(){
         return "helloworld";
     }
